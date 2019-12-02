f7a9d298a9c21d08554f2a5ad1460f435b2cd852
Pavel Dubrova <pashadubrova@gmail.com>
2019-12-02 08:41:25 +0200

    aosp_f5121: Update kernel defconfig name
    We don't use aosp_ prefix in our defconfig files.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- aosp_f5121.mk
+++ aosp_f5121.mk
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-TARGET_KERNEL_CONFIG := aosp_loire_suzu_defconfig
+KERNEL_DEFCONFIG := loire_suzu_defconfig
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/sony/suzu/device.mk)
